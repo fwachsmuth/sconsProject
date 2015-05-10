d75c6172f30c09ba8c68ef74f49f8b5b78c01dd8
Eloi DU BOIS <eloi.du.bois@gmail.com>
2015-05-10 22:04:53 +0200

    sconsProject: minor fix.

--- autoconf/tuttleHost.py
+++ autoconf/tuttleHost.py
@@ -1,12 +1,13 @@
 from _external import *
 from boost import *
+from boost_log import *
 
 tuttleHost = LibWithHeaderChecker(
         'tuttleHost',
         'tuttle/plugin/global.hpp',
         'c++',
         name='tuttleHost',
-        dependencies= [boost],
+        dependencies= [boost, boost_log,],
         )
 
 
